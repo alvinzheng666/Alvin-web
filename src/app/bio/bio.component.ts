a5373312b93b9d185a44f8f5b5026b39bfa8f6a3
hjin-me <hjin.me@gmail.com>
2018-11-08 11:37:28 +0800

    some effect

--- src/app/bio/bio.component.ts
+++ src/app/bio/bio.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bio.component.scss']
 })
 export class BioComponent implements OnInit {
-
-  constructor() { }
+  myEmail: string = '';
+  constructor() {}
 
   ngOnInit() {
+    this.myEmail = '@z@h@e@n@g';
+    this.myEmail = this.myEmail.replace(/@/g, '');
+    this.myEmail = [this.myEmail, 'temple.edu'].join('@');
   }
-
 }
